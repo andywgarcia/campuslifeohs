f10af58f0e0d664196d53570827897e19a9042f1
andywgarcia <andy.w.garcia@gmail.com>
2015-10-02 21:12:40 -0700

    Testing merging i think

--- campuslifeohs/index.php
+++ campuslifeohs/index.php
@@ -17,4 +17,8 @@ define('WP_USE_THEMES', true);
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
 
 
+<<<<<<< d38e9df909d52986dd5c21f4f7ac25934f073301
 #this is proof asdff
+=======
+#this is super proof
+>>>>>>> 32daee9f0e9ff08e1b4e8ad93c5907f269237964
