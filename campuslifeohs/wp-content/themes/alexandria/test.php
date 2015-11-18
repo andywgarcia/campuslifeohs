7a5f8217990816b1dd6676af219e4c9165ca8134
Andy Garcia <andy.w.garcia@gmail.com>
2015-11-18 15:49:01 -0800

    Cleaning up qr code generator

--- campuslifeohs/wp-content/themes/alexandria/test.php
+++ campuslifeohs/wp-content/themes/alexandria/test.php
@@ -27,9 +27,12 @@ get_header(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
 				<?php 
+
 				function createQrPng($firstName,$lastName,$id){
 					$filename = ABSPATH . 'qr/' . $id . '-' . $firstName . '-' . $lastName . '.png';
-					QRcode::png($id, $filename);
+					$size = 8;
+					$margin = 1; 
+					QRcode::png($id, $filename,"L",$size,$margin);
 					
 					//create the blank image
 					$im = imagecreatefrompng($filename);
@@ -43,8 +46,8 @@ get_header(); ?>
 					//can't remember what this does. I think it sets up the canvas with the size
 					$width=imagesx($im);
 					$height=imagesy($im);
-					$newwidth = 400;
-					$newheight = 600;
+					$newwidth = 200;
+					$newheight = 300;
 					$bordersize = 1;
 					$wwidth = $newwidth - 2*$bordersize;
 					$wheight = $newheight - 2*$bordersize;
@@ -59,20 +62,58 @@ get_header(); ?>
 						echo "Image fill failed <br>";
 					}
 
-					//overlay png image of qr code and text
+					$size = 15;
+					$angle = 0;
+					$x = 10;
+					$y = 10;
+						// Create the textbox
+						//$textbox = imagecreatetruecolor($newwidth-50, 30);
+
+						//imagefill($textbox, 0, 0, $black);
+						//imagefill($textbox, 0, 0, $white);
+
+						// Add the text
+						//$tb = imagettftext($textbox, $size, $angle, 0, 0, $black, $fontfile, $text);
+
+
+						
+
+					//array imagettftext ( resource $image , float $size , float $angle , int $x , int $y , int $color , string $fontfile , string $text )
+					//imagecopy ( resource $dst_im , resource $src_im , int $dst_x , int $dst_y , int $src_x , int $src_y , int $src_w , int $src_h )
+					//Put border
 					imagecopy($output,$white_space,$bordersize,$bordersize,0,0,$wwidth,$wheight);
-					imagettftext($output, 12, 0, 20,130, $black, $fontfile, $text);
-					imagecopy($output, $im, (($newwidth-$width)/2), 10, 0, 0, $width, $height);
+					
+					//overlay png image of qr code
+					//imagecopy($output, $im, (($newwidth-$width)/2), 10, 0, 0, $width, $height);
+
+					//overlay text
+					//$x = ceil(($newwidth - $tb[2]) / 2); // lower left X coordinate for text
+					//$y = ceil($newheight*3/4);
+					//imagecopy($output,$textbox,$x,$y,0,0,$width,$height);
+					//imagettftext($output, $size, $angle, $x, $y, $black, $fontfile, $text);
+
+					$bbox = imagettfbbox($size, 0, $fontfile, $text);
+
+					//$x = $bbox[0] + (imagesx($output) / 2) - ($bbox[4] / 2) + 10;
+					//$y = $bbox[1] + (imagesy($output) / 2) - ($bbox[5] / 2) - 5;
+
+					//imagettftext($output, $size, 0, $x, $y, $black, $fontfile, $testext);
+					imagestring($output,1,$x,$y,$text,$black);
+
+
+
+
+
+
 
 					//create the final image
-					imagepng($output,ABSPATH . 'qr/newerimage.png');
-					imagepng($im, ABSPATH. 'qr/newcl.png');
+					imagepng($output,$filename);
 
 					//clean up
 					imagedestroy($output);
 					imagedestroy($im);
 				}
-
+				/*
 				$filename = ABSPATH . 'qr/newcl.png';
 				QRcode::png("YAY!", $filename);
 				
@@ -118,13 +159,16 @@ get_header(); ?>
 				
 				echo "Site url: " . site_url() . "<br>";
 				echo "ABSPATH: " . ABSPATH . "<br>";
-				
+				*/
+				$firstName = "Testfirst";
+				$lastName = "Testlast";
+				$id = 999;
+				$filename = 'qr/' . $id . '-' . $firstName . '-' . $lastName . '.png';
+				createQrPng($firstName,$lastName,$id);
+
 				?>
-				<img src="<?php echo ABSPATH . '/qr/newcl.png' . "<br>"; ?>" />
-				<img src="<?php echo ABSPATH . '/qr/newerimage.png' . "<br>"; ?>" />
 				
-				<img src="<?php echo site_url() . '/qr/newcl.png'; ?>" />
-				<img src="<?php echo site_url() . '/qr/newerimage.png'; ?>" />
+				<img src="<?php echo site_url() . '/' . $filename; ?>" />
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
 					if ( comments_open() || '0' != get_comments_number() )
